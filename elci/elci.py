0c7cf54facf9147ffe1b70bbbebba403dea2231d
Mburibe18 <mburibe9854@gmail.com>
2020-10-20 13:06:03 -0500

    nuevo

---
+++ elci/elci.py
@@ -0,0 +1,15 @@
+import serial
+
+Fs = 1000   #Frecuencia de muestreo en Hz
+t = 5       #segundos de lectura
+
+serial_com = 'COM5'  # seleccionar el COM adecuado
+ser = serial.Serial(serial_com, baudrate = 115200, timeout=10)
+
+for x in range(Fs*t):
+        try:
+            dat = int(ser.readline())
+        except:
+            dat = 0
+        print(dat)
+
